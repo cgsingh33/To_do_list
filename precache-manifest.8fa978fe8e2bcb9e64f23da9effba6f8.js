51f46f2410f335906df26cafb3b1523435ce85c9
chandu03 <chandragupta.singh4@gmail.com>
2020-06-22 21:43:53 +0530

    Updates

---
+++ precache-manifest.8fa978fe8e2bcb9e64f23da9effba6f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93b3036d3682aee269b46e11549b065c",
+    "url": "/To_do_list/index.html"
+  },
+  {
+    "revision": "f6ba72f9b98dbe3d20b5",
+    "url": "/To_do_list/static/css/main.886558f4.chunk.css"
+  },
+  {
+    "revision": "ce7c83e346e5fbe102d3",
+    "url": "/To_do_list/static/js/2.aac49012.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To_do_list/static/js/2.aac49012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6ba72f9b98dbe3d20b5",
+    "url": "/To_do_list/static/js/main.be2c6d76.chunk.js"
+  },
+  {
+    "revision": "dd148bd9c10d14b8c389",
+    "url": "/To_do_list/static/js/runtime-main.9b32f130.js"
+  }
+]);
